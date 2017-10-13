4bb816ddd8b3ad6f57aa828242f22bf4886660f8
Stefan Bethge <stefan+github@lanpartei.de>
2017-10-13 02:36:17 +0200

    Revert "Use Neon on ARM instead of SSE for mopo/utils.h"
    This reverts commit 271a9dfa9fafee306976c24f7f9e4985d62ad208.

--- mopo/src/utils.h
+++ mopo/src/utils.h
@@ -25,8 +25,6 @@
 
 #ifdef __SSE2__
 #include <emmintrin.h>
-#elif defined(__ARM_NEON) || defined(__ARM_FEATURE_SIMD32)
-#include "SSE2NEON.h"
 #else
 #include <algorithm>
 #endif
@@ -57,7 +55,7 @@ namespace mopo {
     const Value value_2pi(2.0 * PI);
     const Value value_neg_one(-1.0);
 
-#if defined(__SSE2__) || defined(__ARM_NEON) || defined(__ARM_FEATURE_SIMD32)
+#ifdef __SSE2__
     inline double min(double one, double two) {
       _mm_store_sd(&one, _mm_min_sd(_mm_set_sd(one),_mm_set_sd(two)));
       return one;
